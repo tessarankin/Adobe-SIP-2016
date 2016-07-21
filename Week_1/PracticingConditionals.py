e6ef9ca68dd6e78de54d00eb575f23aa2b15a089
tessarankin <tessarankin@gmail.com>
2016-07-21 15:15:32 -0700

    Revert "Added files for GWC"
    This reverts commit 4e9f2cb56ad57d2d653b4892ff33c3e69a989663.

--- Week_1/PracticingConditionals.py
+++
@@ -1,6 +0,0 @@
-number = 79
-user_guess = input("Guess the number.")
-if number == int(guess):
-	print("Yeah!")
-
-print (Nope.)
--- Week_2/Shapes 3.py
+++
@@ -1,8 +0,0 @@
-Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> def drawmeasquarepls()
-SyntaxError: invalid syntax
->>> def drawmeasquarepls():
-	pen_colour = red
-	forward(100)
-	right(90)
