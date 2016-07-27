b65e959364da642849bc7c531f99cf74567d4017
tessarankin <tessarankin@gmail.com>
2016-07-27 15:54:11 -0700

    Added  hover to website and started working with bootstrap example

--- Week_5/My_Website/scripts.js
+++ Week_5/My_Website/scripts.js
@@ -1,8 +1,20 @@
-// window.onload = function(){
+$(document).ready(function(){
+    $(".letter").hover(ome, oml);
+    $
+})
 //     alert('hi');
 // }
 
-$(document).read(function(){
-    $("T_text").slideToggle();
-    });
-});
+// $(document).read(function(){
+//     $("T_text").slideToggle();
+//     });
+// });
+
+
+function ome(){
+    $(this).css("background-color", "gainsboro");
+}
+
+function oml(){
+    $(this).css("background-color", "transparent");
+}
